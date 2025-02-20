892d0df991fc57d5dc4de24844614a256d6e3b95
joaofavoretti <contas.joaopedro.favoretti@gmail.com>
2025-02-20 11:52:43 -0300

    Adding a map abstraction

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,48 @@
 #include <raylib.h>
 
 #include "player.hpp"
+#include "map.hpp"
 
 #define RAYLIB_TMX_IMPLEMENTATION
 #include "raylib-tmx.h"
 
 std::unique_ptr<Player> player;
+std::unique_ptr<Map> map;
 
 void Setup() {
   /*player = std::make_unique<Player>((Vector2){10, 0});*/
+  map = std::make_unique<Map>("/archive/Tiled/first-map.tmx");
 }
+
+double t = 0.0f;
 
 void Update(double deltaTime) {
   /*player->Update(deltaTime);*/
+
+  // Move the map in a circle
+  t += deltaTime;
+  Vector2 newPos = {static_cast<float>(100 + sin(t) * 100), static_cast<float>(100 + cos(t) * 100)};
+  map->SetPosition(newPos);
+
+
 }
 
 void Draw() {
   ClearBackground(RAYWHITE);
 
   /*player->Draw();*/
+  map->Draw();
 }
 
 int main() {
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "Sprite Testing Zone");
+  InitWindow(screenWidth, screenHeight, "Sprite Testing Range");
 
-  SetTargetFPS(60);
+  SetTargetFPS(144);
 
   Setup();
-
-  tmx_map *map = LoadTMX("/archive/Tiled/first-map.tmx");
 
   while (!WindowShouldClose()) {
     double deltaTime = GetFrameTime();
@@ -41,12 +52,9 @@ int main() {
 
     BeginDrawing();
     Draw();
-    DrawTMX(map, 0, 0, WHITE);
     DrawFPS(10, 10);
     EndDrawing();
   }
-
-  UnloadTMX(map);
 
   CloseWindow();
 }
---
+++ src/map.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+
+#include "raylib.h"
+#include "tmx.h"
+
+#define RAYLIB_TMX_IMPLEMENTATION
+#include "raylib-tmx.h"
+
+struct Map {
+
+private:
+  std::unique_ptr<tmx_map> map;
+  Vector2 pos = {0, 0};
+
+public:
+
+  Map(const char *filename) {
+    map = std::unique_ptr<tmx_map>(LoadTMX(filename));
+  }
+
+  ~Map() {
+    UnloadTMX(map.get());
+  }
+
+  Vector2 GetPosition() {
+    return pos;
+  }
+
+  void SetPosition(Vector2 position) {
+    pos = position;
+  }
+
+  void Draw() {
+    DrawTMX(map.get(), pos.x, pos.y, WHITE);
+  }
+
+};
