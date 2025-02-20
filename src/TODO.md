213dc4aa64df1a89ffc839a2805adf97d5d7d737
joaofavoretti <contas.joaopedro.favoretti@gmail.com>
2025-02-20 10:37:20 -0300

    Adding simple map loading

--- src/TODO.md
+++ src/TODO.md
@@ -1,6 +1,16 @@
 # TODO Tasks
 
 - Make a map system
+    - Make my own TMX map loading stuff
     - Interactions between different maps
-    - 
+    - Make collisions using the TMX layers
+    - Make the camera system
+    - Make the player move around the map
+
+- Make the entity system
+    - Add an entity manager
+    - Add the enemies to the map
+    - Add a life system
+    - Add projectiles
+    - Add the collisions
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,23 @@
 
 #include "player.hpp"
 
+#define RAYLIB_TMX_IMPLEMENTATION
+#include "raylib-tmx.h"
+
 std::unique_ptr<Player> player;
 
 void Setup() {
-  player = std::make_unique<Player>((Vector2){10, 0});
+  /*player = std::make_unique<Player>((Vector2){10, 0});*/
 }
 
 void Update(double deltaTime) {
-  player->Update(deltaTime);
+  /*player->Update(deltaTime);*/
 }
 
 void Draw() {
   ClearBackground(RAYWHITE);
 
-  player->Draw();
+  /*player->Draw();*/
 }
 
 int main() {
@@ -30,14 +33,20 @@ int main() {
 
   Setup();
 
+  tmx_map *map = LoadTMX("/archive/Tiled/first-map.tmx");
+
   while (!WindowShouldClose()) {
     double deltaTime = GetFrameTime();
     Update(deltaTime);
 
     BeginDrawing();
     Draw();
+    DrawTMX(map, 0, 0, WHITE);
+    DrawFPS(10, 10);
     EndDrawing();
   }
+
+  UnloadTMX(map);
 
   CloseWindow();
 }
